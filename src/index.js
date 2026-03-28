d8b3a2331b59736f13727f79612dd1701ef89f90
Darel <johndarel0914@gmail.com>
2026-03-28 17:56:28 +0800

    Remove index.js bloat

--- src/index.js
+++ src/index.js
@@ -1,19 +1,1 @@
-const projects = [
-    {
-        id: 1,
-        name: 'Abc',
-    },
-    {
-        id: 2,
-        name: 'Efg',
-    },
-]
-const create = () => {
-    const project = projects.find((project) => (project.id = 3))
 
-    return project
-}
-
-let a = create()
-
-console.log(a)
